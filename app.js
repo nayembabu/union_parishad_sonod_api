f647233e28aed4e5d274b3f06a3775ae7bdabaf3
nayembabu <babunayem@gmail.com>
2021-04-29 01:36:27 +0600

    change

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const dateFormate = require('dateformat');
 const cors = require('cors');
 const db_config = require('./db_config');
+
+const now = new Date();
 
 const db = db_config.getDbServiceInstance();
 
@@ -38,9 +42,106 @@ app.get('/default-certificate/:id', (request, response) => {
     })
     .catch(err => {
         return console.log(err);
-    });
-   
+    });   
 }) 
+
+// Insert Global Certificate Info By User-id
+app.post('/insert-certificate', (request, response) => {
+
+    let arrayObj = (`'${request.body.g_certificate_id}', '${request.body.certificate_des}', '${request.body.userId}'`);
+
+     const result = db.insert_certificate_default(arrayObj)
+    
+    result
+    .then(data => {
+        return response.json(data);
+    })
+    .catch(err => {
+        return console.log(err);
+    });   
+})
+
+// Get User Wise All Certificate 
+app.get('/certificate-default', (request, response) => {
+
+    const result = db.getAllUserCertificate()
+    
+    result
+    .then(data => {
+        return response.json(data);
+    })
+    .catch(err => {
+        return console.log(err);
+    });   
+})
+
+// Get User Wise All Certificate 
+app.get('/certificate-default/:id', (request, response) => {
+    let id = request.params.id; 
+
+    const result = db.getAllUserCertificateByID(id)
+    
+    result
+    .then(data => {
+        return response.json(data);
+    })
+    .catch(err => {
+        return console.log(err);
+    });   
+})
+
+// Get All Certificate By Date 
+app.get('/certificate-date', (request, response) => {
+    let todays_day_format = dateFormate(now, 'yyyy-mm-dd');
+    const result = db.getAllCertificateByDate(todays_day_format)
+
+    result
+     .then(data => {
+        response.json(data);
+    })
+    .catch(err => {
+        return console.log(err);
+    });
+})
+
+// New Certificate Issue
+app.post('/certificate-issue', (request, response) => {
+
+    let todays_day_format = dateFormate(now, 'yyyy-mm-dd');
+
+    let arrayObj = (`'${request.body.user_default_certificate_id}', '${request.body.certificate_full_des}', '${request.body.certificate_title}', '${request.body.certificate_id_by_date}', '${todays_day_format}', '${request.body.uniqTimestamp}', ${request.body.person_name}, ${request.body.mother_name}, ${request.body.ward}, ${request.body.village}, ${request.body.post}, ${request.body.thana_s}, ${request.body.nidorbirth}, '${request.body.bn_or_en}', '1', '1', '${request.body.userId}'`);    
+
+    const result = db.insertNewCertificateData(arrayObj)
+
+    result
+    .then(data => {
+        return response.json(data);
+    })
+    .catch(err => {
+        return console.log(err);
+    });    
+})
+
+// Get Issued Certificate By ID
+app.get('/certificate/:id', (request, response) => {
+    let id = request.params.id; 
+
+    const result = db.getCertificateByID(id)
+    
+    result
+    .then(data => {
+        return response.json(data);
+    })
+    .catch(err => {
+        return console.log(err);
+    });   
+}) 
+
+
+
+
+
+
 
 
 
--- db_config.js
+++ db_config.js
@@ -64,6 +64,126 @@ class Dbquery {
         }
     }
 
+    // Query for Insert Certificate Default 
+    async insert_certificate_default(arrayObj) {
+         try {
+            const response = await new Promise((resolve, reject) => {
+                const query = `INSERT INTO certificate_default_by_user (global_default_certificate_iddi, user_default_certificate_full_description, customer_uniq_priiddii) VALUES (${arrayObj});`;
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        } 
+    }
+
+    // Query for Get All Global Data
+    async getAllUserCertificate() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM certificate_default_by_user;";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Query for Get All Global Data
+    async getAllUserCertificateByID(id) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = `SELECT * FROM certificate_default_by_user WHERE user_certificate_default_iidd = ${id};`;
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Query for Get Last Data by date
+    async getAllCertificateByDate(todays_day_format) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query =  `SELECT * 
+                                FROM certificate_entry 
+                                WHERE cer_entry_date = "${todays_day_format}" 
+                                ORDER BY cer_p_iddd DESC 
+                                LIMIT 1;`;
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // New Certificate Inserted
+    async insertNewCertificateData(arrayObj) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = `INSERT INTO certificate_entry (set_user_certificate_default_id, cer_entry, cer_title, cer_id_datewise, cer_entry_date, timestamp, person_name, mother_name, ward, vill, post, thana_s, nidorbirth, bn_or_en, approval, in_or_out, customer_person_uniq_id) VALUES (${arrayObj});`;
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Query for Get Certificate by ID
+    async getCertificateByID(id) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query =  `SELECT * 
+                                FROM certificate_entry 
+                                WHERE cer_p_iddd = ${id};`;
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
+
+
+
+
 
 
 
